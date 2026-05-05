1149747000d677850e42fa2b15ff3efab9502a22
Louay Houimli <louayyhouimli@gmail.com>
2026-05-05 11:24:48 +0100

    add Meuble create form

--- src/Controller/MeubleController.php
+++ src/Controller/MeubleController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Meuble;
+use App\Form\MeubleType;
 use App\Repository\MeubleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,8 +18,28 @@ final class MeubleController extends AbstractController
     {
         $meubles = $repo->findAll();
 
-        return $this->render('meuble/index.html.twig', [
+        return $this->render('admin/meuble/index.html.twig', [
             'meubles' => $meubles
+        ]);
+    }
+
+    #[Route('/admin/meuble/add', name: 'meuble_add')]
+    public function add(Request $request, EntityManagerInterface $em): Response
+    {
+        $meuble = new Meuble();
+
+        $form = $this->createForm(MeubleType::class, $meuble);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($meuble);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_meuble_list');
+        }
+
+        return $this->render('admin/meuble/add.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/MeubleType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categorie;
+use App\Entity\Meuble;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MeubleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('prix')
+            ->add('description')
+            ->add('image')
+            ->add('stock')
+            ->add('categorie', EntityType::class, [
+                'class' => Categorie::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Meuble::class,
+        ]);
+    }
+}
