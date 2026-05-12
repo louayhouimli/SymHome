dedc0d53f5d5d83af6c4b2991035a46e35b5aae0
Louay Houimli <louayyhouimli@gmail.com>
2026-05-12 15:22:15 +0100

    add edit meuble and delete meuble

--- src/Controller/MeubleController.php
+++ src/Controller/MeubleController.php
@@ -58,4 +58,69 @@ final class MeubleController extends AbstractController
             'form' => $form->createView()
         ]);
     }
+
+    #[Route('/admin/meuble/edit/{id}', name: 'meuble_edit')]
+    public function edit(
+        Meuble $meuble,
+        Request $request,
+        EntityManagerInterface $em
+    ): Response {
+
+        $oldImage = $meuble->getImage();
+
+        $form = $this->createForm(MeubleType::class, $meuble);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $imageFile = $form->get('image')->getData();
+
+            if ($imageFile) {
+
+                if ($oldImage && file_exists(
+                    $this->getParameter('kernel.project_dir')
+                    . '/public/uploads/'
+                    . $oldImage
+                )) {
+
+                    unlink(
+                        $this->getParameter('kernel.project_dir')
+                        . '/public/uploads/'
+                        . $oldImage
+                    );
+                }
+
+                $newFilename = uniqid().'.'.$imageFile->guessExtension();
+
+                $imageFile->move(
+                    $this->getParameter('kernel.project_dir').'/public/uploads',
+                    $newFilename
+                );
+
+                $meuble->setImage($newFilename);
+            }
+
+            $em->flush();
+
+            return $this->redirectToRoute('admin_meuble_list');
+        }
+
+        return $this->render('admin/meuble/edit.html.twig', [
+            'form' => $form->createView(),
+            'meuble' => $meuble
+        ]);
+    }
+    #[Route('/admin/meuble/delete/{id}', name: 'meuble_delete')]
+    public function delete(
+        Meuble $meuble,
+        EntityManagerInterface $em
+    ): Response {
+
+        $em->remove($meuble);
+
+        $em->flush();
+
+        return $this->redirectToRoute('admin_meuble_list');
+    }
 }
