748fff07679f7a7ce550820c887ba4622e6f612b
hayouniamine <aammiinneehayouni@gmail.com>
2026-05-14 10:01:05 +0100

    enhance ClientController with search functionality and update client UI

--- src/Controller/Client/ClientController.php
+++ src/Controller/Client/ClientController.php
@@ -4,15 +4,35 @@ namespace App\Controller\Client;
 
 use App\Repository\MeubleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ClientController extends AbstractController
 {
     #[Route('/client', name: 'app_client')]
-    public function listAll(MeubleRepository $MeubleRepository,): Response
+    public function listAll(
+        Request $request,
+        MeubleRepository $meubleRepository
+    ): Response
     {
-        $meubles = $MeubleRepository->findAll();
+        $search = $request->query->get('recherche');
+
+        if ($search) {
+
+            $meubles = $meubleRepository->createQueryBuilder('m')
+                ->join('m.categorie', 'c')
+                ->where('m.nom LIKE :search')
+                ->orWhere('c.libelle LIKE :search')
+                ->setParameter('search', '%'.$search.'%')
+                ->getQuery()
+                ->getResult();
+
+        } else {
+
+            $meubles = $meubleRepository->findAll();
+        }
+
         return $this->render('client/index.html.twig', [
             'meubles' => $meubles,
         ]);
