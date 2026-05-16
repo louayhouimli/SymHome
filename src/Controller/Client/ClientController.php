e3dab1911aeda89e95377ef6919e06ba43993764
hayouniamine <aammiinneehayouni@gmail.com>
2026-05-16 22:22:11 +0100

    added category selection

--- src/Controller/Client/ClientController.php
+++ src/Controller/Client/ClientController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Client;
 
 use App\Entity\Meuble;
+use App\Repository\CategorieRepository;
 use App\Repository\MeubleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,40 +15,44 @@ final class ClientController extends AbstractController
     #[Route('/client', name: 'app_client')]
     public function listAll(
         Request $request,
-        MeubleRepository $meubleRepository
+        MeubleRepository $meubleRepository,
+        CategorieRepository $categorieRepository
     ): Response
     {
         $search = $request->query->get('recherche');
+        $categorieId = $request->query->get('categorie');
+
+        $qb = $meubleRepository->createQueryBuilder('m')
+            ->join('m.categorie', 'c');
 
         if ($search) {
-
-            $meubles = $meubleRepository->createQueryBuilder('m')
-                ->join('m.categorie', 'c')
-                ->where('m.nom LIKE :search')
-                ->orWhere('c.libelle LIKE :search')
-                ->setParameter('search', '%'.$search.'%')
-                ->getQuery()
-                ->getResult();
-
-        } else {
-
-            $meubles = $meubleRepository->findAll();
+            $qb->andWhere('m.nom LIKE :search OR c.libelle LIKE :search')
+            ->setParameter('search', '%'.$search.'%');
         }
+
+        if ($categorieId) {
+            $qb->andWhere('c.id = :cat')
+            ->setParameter('cat', $categorieId);
+        }
+
+        $meubles = $qb->getQuery()->getResult();
 
         return $this->render('client/index.html.twig', [
             'meubles' => $meubles,
+            'categories' => $categorieRepository->findAll()
         ]);
     }
     #[Route('/client/meuble/{id}', name: 'client_meuble_details')]
-    public function details(Meuble $meuble): Response
+    public function details(Meuble $meuble,CategorieRepository $categorieRepository): Response
     {
         return $this->render('client/details.html.twig', [
-            'meuble' => $meuble
+            'meuble' => $meuble,
+            'categories' => $categorieRepository->findAll()
         ]);
     }
     #[Route('/panier', name: 'app_panier')]
-    public function panier(): Response
+    public function panier(CategorieRepository $categorieRepository): Response
     {
-        return $this->render('client/panier.html.twig');
+        return $this->render('client/panier.html.twig',['categories' => $categorieRepository->findAll()]);
     }
 }
